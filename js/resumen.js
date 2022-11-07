8307b55ed4bac5dbfa1c3478b1376dbdc8743942
Elena Mederos <elenamc91@hotmail.com>
2022-11-07 20:10:49 +0100

    Mejorando el diseño del resumen de compra

--- js/resumen.js
+++ js/resumen.js
@@ -11,11 +11,21 @@ vuelo: {id: 41, origen: "Madrid", destino: "Edimburgo", fecha: "2022-10-26", hor
     precioTotal(reserva.pasajeros.length, reserva.vuelo.precio);
 })();
 
+function resumenNumReserva(){
+    var compra = JSON.parse(localStorage.getItem("compraActual"));
+    var text = document.createTextNode(`Código de reserva: ${compra.numReserva}`);
+    var p = document.createElement("p");
+    p.setAttribute("id","numReservaParrafo");
+    p.appendChild(text);
+    document.getElementById("num-reserva").appendChild(p);
+}
+
 function resumenPasajeros(pasajeros) {
     var div = document.getElementsByClassName('resumenpasajeros')[0];
     for(let i = 0; i < pasajeros.length; i++) {
         var p = document.createElement('p');
         p.appendChild(document.createTextNode(`PASAJERO ${i+1}`));
+        p.setAttribute("class","pasajero-title");
         var p2 = document.createElement('p');
         p2.appendChild(document.createTextNode(`Nombre: ${pasajeros[i].nombre} ${pasajeros[i].apellidos} - DNI: ${pasajeros[i].dni}`));
         div.appendChild(p);
@@ -24,19 +34,32 @@ function resumenPasajeros(pasajeros) {
 }
 
 function resumenVuelo(vuelo) {
+    var divSalida = document.createElement("div");
+    var divLlegada = document.createElement("div");
+    divSalida.setAttribute("id","div-salida");
+    divLlegada.setAttribute("id","div-salida");
     var divResumenVuelo = document.getElementsByClassName("resumenvuelo")[0];
     var p1 = document.createElement("p")
     var p2 = document.createElement("p")
     var p3 = document.createElement("p")
     var p4 = document.createElement("p")
-    p1.appendChild(document.createTextNode((`Origen: ${vuelo.origen}`)));
-    p2.appendChild(document.createTextNode((`Destino: ${vuelo.destino}`)));
-    p3.appendChild(document.createTextNode((`Fecha: ${vuelo.fecha}`)));
-    p4.appendChild(document.createTextNode((`Hora: ${vuelo.hora}`)));
-    divResumenVuelo.appendChild(p1)
-    divResumenVuelo.appendChild(p2)
-    divResumenVuelo.appendChild(p3)
-    divResumenVuelo.appendChild(p4)
+    p1.appendChild(document.createTextNode((` ${vuelo.origen}`)));
+    p1.setAttribute("class","destino-origen");
+    p2.appendChild(document.createTextNode((`${vuelo.destino}`)));
+    p2.setAttribute("class","destino-origen");
+    p3.appendChild(document.createTextNode((`${vuelo.fecha} - ${vuelo.hora}h`)));
+    p4.appendChild(document.createTextNode((`${vuelo.fecha} - ${vuelo.horallegada}h`)));
+    
+    divSalida.innerHTML="<p class='titulo-div'>Salida</p>";
+    divSalida.appendChild(p1);
+    divSalida.appendChild(p3);
+
+    divLlegada.innerHTML="<p class='titulo-div'>Llegada</p>";
+    divLlegada.appendChild(p2);
+    divLlegada.appendChild(p4);
+
+    divResumenVuelo.appendChild(divSalida)
+    divResumenVuelo.appendChild(divLlegada)
 }
 
 function precioTotal(numPasajeros, precioBillete) {
